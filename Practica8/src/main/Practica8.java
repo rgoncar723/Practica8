615f28ff7182c948c4f93c7c0e066f4cb14f4d83
rgoncar723 <rgoncar723@g.edcuaand.es>
2025-12-05 14:30:51 +0100

    1: Creación del proyecto, comentario inicial (borrar despues)

---
+++ Practica8/src/main/Practica8.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Practica8 {
+	public void show() {
+		System.out.println("Pasa amegho!");
+	}
+
+	public static void main(String[] args) {
+		new Practica8().show();
+
+	}
+
+}
