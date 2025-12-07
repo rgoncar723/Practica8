5238d1fe90d7e45c78daf62128bb8daff2e08db3
smefrag2310 <smefrag2310@g.educaand.es>
2025-12-07 22:10:11 +0100

    Resolviendo la cagada que acabo de realizar, perdón

--- Practica8/src/main/Practica8.java
+++ Practica8/src/main/Practica8.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Practica8 {
 	Scanner keyboard = new Scanner(System.in);
 	public void show() {
-		int menu,numero,number1,number2;
+		int menu,numero;
 		int[]numbers;
 		System.out.printf("-----Bienvenid@s al intento numero uno de la calculadora----");
 		do {
@@ -19,23 +19,12 @@ public class Practica8 {
 				numbers= datosUsuario(numero);
 				System.out.printf("La suma de los numeros es %d\n",sumar(numbers));
 			
-			}if(menu==4) {
-				
-				System.out.println("Introduce los dos números enteros que quieres dividir(será n1/n2):");
-				number1=keyboard.nextInt();number2=keyboard.nextInt();
-				
-					System.out.printf("La división de %d y %d es igual a %d\n",number1,number2,divide(number1,number2));
-			}if(menu==5) {
-				System.out.println("Introduce dos números enteros, el primero la base y el segundo el exponente: ");
-				number1=keyboard.nextInt();number2=keyboard.nextInt();
-				
-				System.out.printf("El número %d elevado a %d es igual a %d\n",number1,number2,Pow(number1,number2));
 			}
 			//Y Asi sucesivamente 
 
 		}while(menu!=6); 
 		System.out.println(" Saliendo del programa");
-	
+		
 	}
 	
 	public int[] datosUsuario(int numero) {
@@ -55,16 +44,7 @@ public class Practica8 {
 		}
 		return result;
 	}
-	public int divide(int n1,int n2) {
-		int result;
-		result=n1/n2;
-		return result;
-	}
-	public int Pow(int n1,int n2) {
-		int result;
-		result=(int)Math.pow(n1, n2);
-		return result;
-	}
+	
 	
 	public static void main(String[] args) {
 		new Practica8().show();
