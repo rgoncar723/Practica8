0c28b8a855206b0e2160aa12011f75eacd08e6f9
smefrag2310 <smefrag2310@g.educaand.es>
2025-12-07 20:39:20 +0100

    Subiendo la división y potencia de la calculadora(dos números)
    Incluyendo las funciones de divide y Pow, los condicionales de if
    menu==4,5, y las respectivas operaciones y mensajes por consola

--- Practica8/src/main/Practica8.java
+++ Practica8/src/main/Practica8.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Practica8 {
 	Scanner keyboard = new Scanner(System.in);
 	public void show() {
-		int menu,numero;
+		int menu,numero,number1,number2;
 		int[]numbers;
 		System.out.printf("-----Bienvenid@s al intento numero uno de la calculadora----");
 		do {
 			//Ver si esto puede ponerse como funcion aparte 
-			System.out.printf("\n Menu: \n 1.Sumar \n 2. Restar \n 3. Multiplicar \n 4. Dividir dos numeros \n 5. Potencia de dos numeros \n Por favor escribe el numero de la operacion que desea realizar");
+			System.out.printf("\n Menu: \n 1.Sumar \n 2. Restar \n 3. Multiplicar \n 4. Dividir dos numeros \n 5. Potencia de dos numeros\n 6. Salir del programa \n Por favor escribe el numero de la operacion que desea realizar: ");
 			menu = keyboard.nextInt();
 			if(menu==1) {
 				//Esto seguramente se pueda poner como una funcion
@@ -18,12 +18,23 @@ public class Practica8 {
 				numero=keyboard.nextInt();
 				numbers= datosUsuario(numero);
 				System.out.printf("La suma de los numeros es %d\n",sumar(numbers));
+			
+			}if(menu==4) {
 				
+				System.out.println("Introduce los dos números enteros que quieres dividir(será n1/n2):");
+				number1=keyboard.nextInt();number2=keyboard.nextInt();
+				
+					System.out.printf("La división de %d y %d es igual a %d\n",number1,number2,divide(number1,number2));
+			}if(menu==5) {
+				System.out.println("Introduce dos números enteros, el primero la base y el segundo el exponente: ");
+				number1=keyboard.nextInt();number2=keyboard.nextInt();
+				
+				System.out.printf("El número %d elevado a %d es igual a %d\n",number1,number2,Pow(number1,number2));
 			}
 			//Y Asi sucesivamente 
 
-		}while(menu!=5); 
-		System.out.println("Saliendo del programa");
+		}while(menu!=6); 
+		System.out.println(" Saliendo del programa");
 	
 	}
 	
@@ -44,6 +55,17 @@ public class Practica8 {
 		}
 		return result;
 	}
+	public int divide(int n1,int n2) {
+		int result;
+		result=n1/n2;
+		return result;
+	}
+	public int Pow(int n1,int n2) {
+		int result;
+		result=(int)Math.pow(n1, n2);
+		return result;
+	}
+	
 	public static void main(String[] args) {
 		new Practica8().show();
 
